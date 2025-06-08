49e8257c34e0f61c776e0dc6038d3aecd04bf429
GantangSatria <gantang610@gmail.com>
2025-06-08 11:45:39 +0700

    fix(ui): dont ask :(

--- app/src/main/java/com/gsatria/myapplication/ui/auth/Onboarding.kt
+++ app/src/main/java/com/gsatria/myapplication/ui/auth/Onboarding.kt
@@ -1,15 +1,19 @@
 package com.gsatria.myapplication.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gsatria.myapplication.R
 
 class Onboarding : AppCompatActivity() {
 
-    private lateinit var
+    private lateinit var btnLogin: AppCompatButton
+    private lateinit var btnRegister: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,18 @@ class Onboarding : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+
+        btnLogin = findViewById(R.id.loginButton)
+        btnRegister = findViewById(R.id.registerText)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
