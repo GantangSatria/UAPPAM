a0549b2ee8a4d99184aefaef0335a5dd17d8afaa
GantangSatria <gantang610@gmail.com>
2025-06-08 11:32:59 +0700

    feat: slicing kinda done, just need to add some details and fix the linear layout

--- app/src/main/java/com/gsatria/myapplication/ui/auth/Onboarding.kt
+++ app/src/main/java/com/gsatria/myapplication/ui/auth/Onboarding.kt
@@ -8,14 +8,17 @@ import androidx.core.view.WindowInsetsCompat
 import com.gsatria.myapplication.R
 
 class Onboarding : AppCompatActivity() {
+
+    private lateinit var
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
